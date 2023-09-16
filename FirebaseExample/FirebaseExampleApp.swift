fa504ac9e48eb47cd773dd863dc32fdaa5957b59
AlexParreira <alexander.o.parreira@gmail.com>
2023-09-16 14:12:47 -0300

    Initial Commit

---
+++ FirebaseExample/FirebaseExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseExampleApp.swift
+//  FirebaseExample
+//
+//  Created by Alexander Parreira on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
